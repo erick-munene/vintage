8c71dfdc0c7e66ea4933d432b77e81c36769e2b6
Erick-Munene <munenewahome@gmail.com>
2016-06-09 15:09:10 +0300

    added jquery sticky nav

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation()
+
+
+
+$(document).ready(function () {
+    var stickyNavTop = $('.menu').offset().top;
+
+    var stickyNav = function () {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) {
+            $('.menu').addClass('sticky');
+        } else {
+            $('.menu').removeClass('sticky');
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function () {
+        stickyNav();
+    });
+});
